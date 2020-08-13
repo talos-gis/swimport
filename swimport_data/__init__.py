206b34fd65f9a9ccd5a1deb6c6425a164a541b9b
Idan Miara <idan@miara.com>
2020-08-13 18:32:26 +0300

    .gitignore udpated swimport_data/__init__.py - swimport info

---
+++ swimport_data/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '3.0.12'
+__author__ = 'Ben Avrahami'
+__author_email__ = "avrahami.ben@gmail.com"
+__license__ = "MIT"
+__url__ = r"https://github.com/talos-gis/swimport"
+__name__ = 'swimport'
