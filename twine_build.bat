01259eb2c508860b3bdaaf33e6f0e7a1ee9b29a0
Idan Miara <idan@miara.com>
2021-01-23 12:54:02 +0200

    twine_build.bat update

--- twine_build.bat
+++ twine_build.bat
@@ -1,9 +1,16 @@
+:: python -m pip install twine wheel
+
 :: delete old dists
 rmdir /s/q dist
 rmdir /s/q build
 
 :: creating the package
-python setup.py sdist bdist_wheel
+python setup.py bdist_wheel
+
+:: test the dist via twine
+python -m twine check dist/*.whl
 
 :: uploading the dist via twine
-python -m twine upload dist/*.gz dist/*.whl
+set twine_url=
+if %1x neq x set twine_url=--repository-url https://test.pypi.org/legacy/
+python -m twine upload %twine_url% dist/*.whl
